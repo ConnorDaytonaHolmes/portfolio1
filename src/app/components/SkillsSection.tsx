983eb201167f303f5cf84b91eacdbef17fd1a834
Connor Holmes <connorholmes.419@gmail.com>
2025-06-20 18:59:35 +0800

    skill tooltips

--- src/app/components/SkillsSection.tsx
+++ src/app/components/SkillsSection.tsx
@@ -13,17 +13,17 @@ export default function SkillsSection() {
           <SkillIcon icon='/typescript.svg' alt='TypeScript' />
           <SkillIcon icon="/csharp.svg" alt="C#" />
           <SkillIcon icon="/sql.svg" alt="SQL" />
+          <SkillIcon icon="/python.svg" alt="Python" />
           <SkillIcon icon="/react.svg" alt="React" />
-          <SkillIcon icon="/java.svg" alt="Java" />
           <SkillIcon icon="/bash.svg" alt="Bash" />
+          <SkillIcon icon="/java.svg" alt="Java" />
           <SkillIcon icon="/rust.svg" alt="Rust" />
           <SkillIcon icon="/go.svg" alt="Go" />
           <SkillIcon icon="/cpp.svg" alt="C++" />
-          <SkillIcon icon="/python.svg" alt="Python" />
         </div>
-        <hr className='fade-in-section bg-gray-200'/>
+        <hr className='fade-in-section delay-100 bg-gray-200'/>
         { /* Other Technologies */ }
-        <div className="fade-in-section flex flex-wrap justify-center gap-8">
+        <div className="fade-in-section delay-200 flex flex-wrap justify-center gap-8">
           <SkillIcon icon="/git.svg" alt="Git" />
           <SkillIcon icon='/azure.svg' alt='Azure' />
           <SkillIcon icon="/docker.svg" alt="Docker" />
@@ -37,10 +37,12 @@ export default function SkillsSection() {
 function SkillIcon({ icon, alt }: { icon: string; alt: string }) {
   const size = 48;
   return (
-    <div className="flex flex-col items-center justify-center group">
-      {/* <Tooltip text={alt} /> */}
-      <div className="transition-transform duration-200 transform group-hover:scale-175">
+    <div className="flex flex-col items-center justify-center group relative">
+      <div className="transition-transform duration-300 transform group-hover:scale-175">
         <Image src={icon} alt={alt} width={size} height={size} />
+      </div>
+      <div className="absolute top-full mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-sm text-gray-300 text-center whitespace-nowrap">
+        {alt}
       </div>
     </div>
   );
