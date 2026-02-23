d3db35d78976ae2e2f60aff252177d59bb4dccd0
Connor Holmes <connor@redisoftware.com.au>
2026-02-24 07:51:12 +0800

    return early if no key

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -18,6 +18,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
+    if(!process.env.RESEND_API_KEY) {
+      return NextResponse.json({ error: "Resend API key not set" }, { status: 500 });
+    }
     const resend = new Resend(process.env.RESEND_API_KEY);
     const { name, email, phone, message } = await req.json();
     const { data, error } = await resend.emails.send({
