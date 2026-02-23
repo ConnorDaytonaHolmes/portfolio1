e72d0227b5eab22054268d4f1d3e2f8911ae9737
Connor Holmes <connor@redisoftware.com.au>
2026-02-24 06:56:25 +0800

    just resend

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -4,6 +4,8 @@ import EmailTemplate from '../../components/email/EmailTemplate';
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(req: NextRequest) {
   try {
     const { name, email, phone, message } = await req.json();
@@ -13,7 +15,6 @@ export async function POST(req: NextRequest) {
   }
 }
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
 
 // export async function POST(req: NextRequest) {
 //   try {
