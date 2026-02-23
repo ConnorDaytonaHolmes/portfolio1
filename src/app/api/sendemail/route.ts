6dbd5b5f36332893ec0eeb7f60dc994451958be0
Connor Holmes <connor@redisoftware.com.au>
2026-02-24 07:33:52 +0800

    try that

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -5,33 +5,34 @@ import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
 
-export async function POST(req: NextRequest) {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-  try {
-    const { name, email, phone, message } = await req.json();
-    return NextResponse.json({ name, email, phone, message }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
-}
-
-
 // export async function POST(req: NextRequest) {
+//   const resend = new Resend(process.env.RESEND_API_KEY);
 //   try {
 //     const { name, email, phone, message } = await req.json();
-//     const { data, error } = await resend.emails.send({
-//       from: 'Connor Holmes Software <outreach@mail.connorholmes.software>',
-//       to: ['connorholmes.419@gmail.com'],
-//       subject: `New message from ${name}`,
-//       react: EmailTemplate({ name, email, phone, message }),
-//     });
-
-//     if (error) {
-//       return NextResponse.json({ error }, { status: 400 });
-//     }
-
-//     return NextResponse.json(data);
+//     return NextResponse.json({ name, email, phone, message }, { status: 200 });
 //   } catch (error) {
 //     return NextResponse.json({ error }, { status: 500 });
 //   }
 // }
+
+
+export async function POST(req: NextRequest) {
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const { name, email, phone, message } = await req.json();
+    const { data, error } = await resend.emails.send({
+      from: 'Connor Holmes Software <outreach@mail.connorholmes.software>',
+      to: ['connorholmes.419@gmail.com'],
+      subject: `New message from ${name}`,
+      react: EmailTemplate({ name, email, phone, message }),
+    });
+
+    if (error) {
+      return NextResponse.json({ error }, { status: 400 });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
