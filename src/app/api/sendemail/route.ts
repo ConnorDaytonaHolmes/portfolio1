0ac47bcb6b9497f94dee9a5adcb135e2d9c8f996
Connor Holmes <connor@redisoftware.com.au>
2026-02-24 11:27:07 +0800

    error msg

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -4,18 +4,6 @@ import EmailTemplate from '../../components/email/EmailTemplate';
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-
-// export async function POST(req: NextRequest) {
-//   const resend = new Resend(process.env.RESEND_API_KEY);
-//   try {
-//     const { name, email, phone, message } = await req.json();
-//     return NextResponse.json({ name, email, phone, message }, { status: 200 });
-//   } catch (error) {
-//     return NextResponse.json({ error }, { status: 500 });
-//   }
-// }
-
-
 export async function POST(req: NextRequest) {
   try {
     if(!process.env.RESEND_API_KEY) {
@@ -36,6 +24,6 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: { message: "Failed to send email", inner: error } }, { status: 500 });
   }
 }
