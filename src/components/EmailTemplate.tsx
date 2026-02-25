f763bbd7ee520d2fd2c4c2dda9e6813baf758cbd
Connor Holmes <connorholmes.419@gmail.com>
2026-02-25 22:32:51 +0800

    try react component again

---
+++ src/components/EmailTemplate.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+interface EmailTemplateProps {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
+export function EmailTemplate({ name, email, phone, message }: EmailTemplateProps) {
+  return (
+    <div className='bg-gray-100'>
+      <div className="text-white bg-gray-900 p-4 rounded-lg">
+        <h1 className='text-2xl font-bold my-2'>{name} has reached out!</h1>
+
+        <p className='my-2'>{message}</p>
+
+        {phone && (
+          <span className='flex flex-row items-center gap-4 my-1'>
+            <p>📞</p>
+            <p>{phone}</p>
+          </span>
+        )}
+
+        {email && (
+          <span className='flex flex-row items-center gap-4 my-1'>
+            <p>✉️</p>             
+            <p>{email}</p>
+            
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
