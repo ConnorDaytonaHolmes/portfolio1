e8883fc29f494824bcdeed4b46a18e284532949a
Connor Holmes <connor@redisoftware.com.au>
2026-02-25 10:51:01 +0800

    Revert "try that"
    This reverts commit 4c7f51b8e750157b6f30f6847a4f078606689149.

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -4,26 +4,26 @@ import EmailTemplate from '../../components/email/EmailTemplate';
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     if(!process.env.RESEND_API_KEY) {
-//       return NextResponse.json({ error: "Resend API key not set" }, { status: 500 });
-//     }
-//     const resend = new Resend(process.env.RESEND_API_KEY);
-//     const { name, email, phone, message } = await req.json();
-//     const { data, error } = await resend.emails.send({
-//       from: 'Connor Holmes Software <outreach@mail.connorholmes.software>',
-//       to: ['connorholmes.419@gmail.com'],
-//       subject: `New message from ${name}`,
-//       react: EmailTemplate({ name, email, phone, message }),
-//     });
+export async function POST(req: NextRequest) {
+  try {
+    if(!process.env.RESEND_API_KEY) {
+      return NextResponse.json({ error: "Resend API key not set" }, { status: 500 });
+    }
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const { name, email, phone, message } = await req.json();
+    const { data, error } = await resend.emails.send({
+      from: 'Connor Holmes Software <outreach@mail.connorholmes.software>',
+      to: ['connorholmes.419@gmail.com'],
+      subject: `New message from ${name}`,
+      react: EmailTemplate({ name, email, phone, message }),
+    });
 
-//     if (error) {
-//       return NextResponse.json({ error }, { status: 400 });
-//     }
+    if (error) {
+      return NextResponse.json({ error }, { status: 400 });
+    }
 
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return NextResponse.json({ error: { message: "Failed to send email", inner: error } }, { status: 500 });
-//   }
-// }
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error: { message: "Failed to send email", inner: error } }, { status: 500 });
+  }
+}
